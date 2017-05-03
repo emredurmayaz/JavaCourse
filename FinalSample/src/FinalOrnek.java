84f0e4007f5c361fa86189f794801a080c6a38c3
emredurmayaz <emredurmayaz@hotmail.com>
2017-05-03 14:41:35 +0300

    FinalCommit

---
+++ FinalSample/src/FinalOrnek.java
@@ -0,0 +1,11 @@
+
+public class FinalOrnek {
+	
+	///final deðiþeknlere ya tanýmlandýklarý yerde yada constructor içinde yapýlmaldýr
+       public final int cID;
+      
+	public FinalOrnek(int cID) {
+		
+		this.cID = cID;
+	}
+}
---
+++ FinalSample/src/FinalOrnekTest.java
@@ -0,0 +1,10 @@
+
+public class FinalOrnekTest {
+
+	public static void main(String[] args) {
+		FinalOrnek ornek=new FinalOrnek(0);
+		System.out.println(ornek.cID);
+
+	}
+
+}
